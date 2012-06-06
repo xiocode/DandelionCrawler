24e773eb6030f98bc66ffb6ca636ca06adf398b0
Tony.Shao <xiocode@gmail.com>
2012-06-06 12:06:10 +0800

    init

---
+++ AutoTokenGenerator.py
@@ -0,0 +1,29 @@
+'''
+Created on 2012-6-4
+
+@author: windows
+'''
+
+class AutoTokenGenerator(object):
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    
+    def generatorToken(self, site_id):
+        '''
+        Auto Generator a valid token for different site platform.
+        '''
+    
+    def __getValidToken(self):
+        '''
+        get a valid token from database.
+        '''
+    
+    def __updateToken(self):
+        '''
+        update a invalid token
+        '''
+    
+    
